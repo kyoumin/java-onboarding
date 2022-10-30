fece9b3bb93df19654117b1d7147dd3124457b05
kyoumin <cherish__97@naver.com>
2022-10-30 20:06:07 +0900

    ADD:2번ã3번 문제 풀이

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
-    public static String solution(String cryptogram) {
+    public static void main(String[] args) {
+        System.out.println(solution("browoanoommnaon"));
+    }
+    public static Stack<String> solution(String cryptogram) {
+
         String answer = "answer";
-        return answer;
+
+        Stack<String> stack = new Stack<>();
+        String[] n = cryptogram.split("");
+
+        for(int i=0;i<n.length-1;i++) {
+
+            stack.push(n[i]);
+            //stack.push(n[i+1]);
+            System.out.println(stack);
+
+            if (n[i].equals(n[i +1])) {
+                System.out.println("같");
+                System.out.println(stack.pop());
+                System.out.println(stack.pop());
+            }
+            //else
+               // continue;
+
+        }
+        System.out.println(stack.toString());
+
+        return stack;
+
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+    public static void main(String[] args) {
+        System.out.println(solution(33));
+    }
+
+    //13 : 3 6 9 13
+    //33 : 3,6,9,13,16,19,23,26,29,30,31,32,33 :14
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=1;i<number+1;i++) {
+            String temp = Integer.toString(i); //3
+            String[] n = temp.split(""); //3
+
+            for(int j = 0;j<n.length;j++) {
+                if (n[j].equals("3") || n[j].equals("6") || n[j].equals("9"))
+                    answer ++;
+            }
+        }
         return answer;
     }
 }
